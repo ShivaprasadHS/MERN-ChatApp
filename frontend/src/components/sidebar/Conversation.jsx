8ec308f74738428fd6e2b8ae51ed1e04809b8349
ShivaprasadHS <shivaprasad110420@gmail.com>
2024-08-19 17:26:51 +0530

    2nd part completed - ui design

---
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Conversation = () => {
+  return <>
+  <div className='flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer'>
+    <div className='avatar online'>
+        <div className='w-12 rounded-full'>
+            <img src="https://www.iconfinder.com/icons/172628/user_male_icon" alt="user" />
+        </div>
+    </div>
+    <div className='flex flex-col flex-1'>
+        <div className='flex gap-3 justify-between'>
+            <p className='font-bold text-gray-500'>shivu</p>
+            <span className='text-x1'>🤣</span>
+        </div>
+    </div>
+  </div>
+  <div className='divider my-0 py-0 h-1'/>
+  </>;
+};
+
+export default Conversation;
