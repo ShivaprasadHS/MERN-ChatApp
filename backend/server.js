fdfb4357ee8420962167d47677fa891db8c8d616
ShivaprasadHS <shivaprasad110420@gmail.com>
2024-08-20 17:15:39 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express"; // package imports 
 import dotenv  from "dotenv";// package imports
 import cookieParser from "cookie-parser";
@@ -14,6 +15,8 @@ import { app, server } from "./socket/socket.js";
 // const app = express(); // variables
 const PORT = process.env.PORT || 5000; // we will get it from process.env or from port 5000
 
+const __dirname = path.resolve();
+
 dotenv.config(); //.envConfig
 
 app.use(express.json()); // The express.json() middleware should be added before your routes to ensure that incoming request bodies are properly parsed before they reach your route handlers
@@ -22,6 +25,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes); //middleware
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 
  // to parse the incoming request with JSON payloads (from req.body)
 // app.get("/", (req, res) => {
